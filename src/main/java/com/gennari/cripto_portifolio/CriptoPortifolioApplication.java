e0d7581c9a3a8ce7dedb286d8a01f358bce8aeb8
LucasGennari-PG <156801273+LucasGennari-PG@users.noreply.github.com>
2025-02-23 09:27:52 -0300

    first commit

---
+++ src/main/java/com/gennari/cripto_portifolio/CriptoPortifolioApplication.java
@@ -0,0 +1,13 @@
+package com.gennari.cripto_portifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoPortifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoPortifolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gennari/cripto_portifolio/CriptoPortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gennari.cripto_portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoPortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
